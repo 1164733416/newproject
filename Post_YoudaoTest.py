d4b22f510f2e4a4ac29474cfdb812760b343142f
1164733416 <luyanpeng321@126.com>
2020-04-07 10:07:49 +0800

    作业4 22-陆演鹏 finish salt

--- Post_YoudaoTest.py
+++ Post_YoudaoTest.py
@@ -5,15 +5,18 @@ from get_post import *
 
 
 class YoudaoPost(unittest.TestCase):
-
+    def test_something(self):
+        self.assertEqual(True, True)
     def test_get_ts(self):
-        # import time
-        # t = time.time()
-        # ts = str(int(round(t * 1000)))
-        # print(ts)
-        get_ts =mock.Mock(return_value='1585622797941')
+        get_ts =mock.Mock(return_value='1584683853810')
+        self.assertEquals('1584683853810', get_ts())
+    def test_get_salt(self):
+        get_salt =mock.Mock(return_value='15846838538107')
+        self.assertEquals('15846838538107',get_salt())
+    def test_get_sign(self):
+        self.assertEquals('b4f6d46d7883e2dbb18cd92cbaa411b7',get_sign())
 
-        self.assertEquals('1585622797941', get_ts())
+
 
 
 if __name__ == '__main__':
--- get_post.py
+++ get_post.py
@@ -1,18 +1,29 @@
 import requests
-
+import time
 
 
 def get_translate_date(word=None):
     url = 'http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule'
 
-    def get_ts():
-        return '1584683853810'
 
-    def get_sign():
-        return 'b4f6d46d7883e2dbb18cd92cbaa411b7'
+def get_ts():
+    t = time.time()
+    ts = str(int(round(t * 1000)))
+    return ts
+# '1584683853810'
 
-    def get_salt():
-        return '15846838538107'
+def get_sign():
+    return 'b4f6d46d7883e2dbb18cd92cbaa411b7'
+
+
+def get_salt():
+    s = str(random.randint(0, 10))
+    t = get_ts()
+    # print('random = ',s)
+    # print('ts=',t)
+    # print('salt=',t+s)
+    return t + s
+    # '15846838538107'
 
     form_date = {
         'i': ' 我爱中国',
@@ -26,8 +37,8 @@ def get_translate_date(word=None):
         'bv': ' c3d953d5f079ad93810ed7b4b0f76df6',
         'doctype': ' json',
         'version': '2.1',
-        'keyfrom':'fanyi.web',
-        'action':'FY_BY_REALTlME',
+        'keyfrom': 'fanyi.web',
+        'action': 'FY_BY_REALTlME',
     }
     # 请求表单数据
 
@@ -37,6 +48,3 @@ def get_translate_date(word=None):
     # 打印翻译后的数据
     if __name__ == '__main__':
         get_translate_date('我爱数据')
-
-
-
