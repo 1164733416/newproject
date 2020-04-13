abe26feaa866ce89b6977394d5cb7931025dd5d9
1164733416 <luyanpeng321@126.com>
2020-04-13 15:39:13 +0800

    作业5 22-陆演鹏 add post youdao

--- Post_YoudaoTest.py
+++ Post_YoudaoTest.py
@@ -7,16 +7,18 @@ from get_post import *
 class YoudaoPost(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)
+
     def test_get_ts(self):
-        get_ts =mock.Mock(return_value='1584683853810')
+        get_ts = mock.Mock(return_value='1584683853810')
         self.assertEquals('1584683853810', get_ts())
+
     def test_get_salt(self):
-        get_salt =mock.Mock(return_value='15846838538107')
-        self.assertEquals('15846838538107',get_salt())
+        get_salt = mock.Mock(return_value='15846838538107')
+        self.assertEquals('15846838538107', get_salt())
+
     def test_get_sign(self):
-        self.assertEquals('b4f6d46d7883e2dbb18cd92cbaa411b7',get_sign())
-
-
+        get_sign = mock.Mock(return_value='b4f6d46d7883e2dbb18cd92cbaa411b7')
+        self.assertEquals('b4f6d46d7883e2dbb18cd92cbaa411b7', get_sign())
 
 
 if __name__ == '__main__':
--- get_post.py
+++ get_post.py
@@ -1,50 +1,94 @@
-import requests
+import random
 import time
+import requests
 
 
-def get_translate_date(word=None):
-    url = 'http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule'
+# url = 'http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule'
+#  content = '我爱中国'
 
 
-def get_ts():
-    t = time.time()
-    ts = str(int(round(t * 1000)))
-    return ts
-# '1584683853810'
+class Youdao():
+    def __init__(self, content):
+        self.content = content
+        self.url = 'http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule'
+        self.ts = self.get_ts()
+        self.salt = self.get_salt()
+        self.sign = self.get_sign()
 
-def get_sign():
-    return 'b4f6d46d7883e2dbb18cd92cbaa411b7'
+    def get_ts(self):
+        t = time.time()
+        ts = str(int(round(t * 1000)))
+        print(ts)
+        return ts
+
+    # '1584683853810'
+
+    def get_md5(self, value):
+        import hashlib
+        m = hashlib.md5()
+        m.update(value.encode("utf-8"))
+        return m.hexdigest()
+
+    # def get_content(self):
+    #     return content
+
+    def get_sign(self):
+        i = self.salt
+        e = self.content
+        s = 'fanyideskweb' + e + i + "Nw(nmmbP%A-r6U3EUn]Aj"
+        # print('s=', s)
+        return self.get_md5(s)
+        # return 'b4f6d46d7883e2dbb18cd92cbaa411b7'
+
+    def get_salt(self):
+        s = str(random.randint(0, 10))
+        t = self.ts
+        # print('random = ',s)
+        # print('ts=',t)
+        # print('salt=',t+s)
+        return t + s
+        # '15846838538107'
+
+    def yield_form_data(self):
+        form_date = {
+            'i': self.content,
+            'from': 'AUTO',
+            'to': 'AUTO',
+            'smartresult': 'dict',
+            'client': 'fanyideskweb',
+            'salt': self.salt,
+            'sign': self.sign,
+            'ts': self.ts,
+            'bv': 'c3d953d5f079ad93810ed7b4b0f76df6',
+            'doctype': 'json',
+            'version': '2.1',
+            'keyfrom': 'fanyi.web',
+            'action': 'FY_BY_REALTlME',
+        }
+        return form_date
+
+    def get_headers(self):
+        headers = {
+            'Cookie': 'OUTFOX_SEARCH_USER_ID=-1749741278@10.108.160.19; JSESSIONID=aaauT_-zYnOeR5k6WuYfx; OUTFOX_SEARCH_USER_ID_NCOO=322638183.72189075; ___rl__test__cookies=1586761009751',
+            'Referer': 'http://fanyi.youdao.com/',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36 OPR/67.0.3575.53',
+        }
+        return headers
+
+    def headers_one(self):
+        print(self.get_headers())
+
+    def form_data_one(self):
+        print(self.yield_form_data())
+
+    def fanyi(self):
+        response = requests.post(self.url, data=self.yield_form_data(), headers=self.get_headers())
+        return response.text
 
 
-def get_salt():
-    s = str(random.randint(0, 10))
-    t = get_ts()
-    # print('random = ',s)
-    # print('ts=',t)
-    # print('salt=',t+s)
-    return t + s
-    # '15846838538107'
+if __name__ == '__main__':
+    # print(form_date)
+    # print(get_headers())
+    youdao = Youdao('雪碧')
+    print(youdao.fanyi())
 
-    form_date = {
-        'i': ' 我爱中国',
-        'from': ' AUTO',
-        'to': ' AUTO',
-        'smartresult': ' dict',
-        'client': ' fanyideskweb',
-        'salt': get_salt(),
-        'sign': get_sign(),
-        'ts': get_ts(),
-        'bv': ' c3d953d5f079ad93810ed7b4b0f76df6',
-        'doctype': ' json',
-        'version': '2.1',
-        'keyfrom': 'fanyi.web',
-        'action': 'FY_BY_REALTlME',
-    }
-    # 请求表单数据
-
-    response = requests.post(url, data=payload)
-    # 将JSON格式字符串转字典
-
-    # 打印翻译后的数据
-    if __name__ == '__main__':
-        get_translate_date('我爱数据')
